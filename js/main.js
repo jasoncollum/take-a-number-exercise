1f26637028e27ef443ae798eb006bed6662e16a0
Jason Collum <jasoncollum@gmail.com>
2019-04-12 10:45:09 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+console.log('main.js');
+
+let bandNumber = 0;
+
+const takeNumber = function(band) {
+    bandNumber++;
+    return `${bandNumber}. ${band}`;
+}
+
+const scum = takeNumber("Galactic Scum")
+console.log(scum)  // This should print "1. Galactic Scum" in the console
+
+const under = takeNumber("Underdogs")
+console.log(under)  // This should print "2. Underdogs" in the console
